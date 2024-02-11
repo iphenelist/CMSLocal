b43903ddacbba39356c624acb71e16c839fde0f2
iphenelist <innocentphenelist@gmail.com>
2024-02-11 16:19:39 +0300

    first commit

---
+++ cmslocal/cmslocal/doctype/offerings/offerings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, innocent PM and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Offerings', {
+	// refresh: function(frm) {
+
+	// }
+});
